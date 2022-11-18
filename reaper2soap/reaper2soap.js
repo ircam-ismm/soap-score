55d3af5e31214a385a77a4a0cb90e3bde081a4ff
Etienne <etioud@gmail.com>
2022-11-18 17:31:46 +0100

    reaper2soap

--- reaper2soap/reaper2soap.js
+++ reaper2soap/reaper2soap.js
@@ -1,14 +1,56 @@
 let midiParser  = require('midi-parser-js');
 let fs = require('fs')
 
+
+function computeMetricTime(prevUpper, prevLower, triggerTime) {
+  // input 4 4 16
+  const beatTime = 4 / prevLower;
+  const measureTime = beatTime * prevUpper;
+  const triggerMeasure = (triggerTime / measureTime);
+  // console.log(prevUpper, prevLower, triggerTime, triggerMeasure);
+  return triggerMeasure;
+
+  // expected output 5
+  // input 3 8 1.5
+  // expected output 2
+
+}
+
+
 // read a .mid binary (as base64)
-fs.readFile('./midi_export.mid', 'base64', function (err,data) {
+fs.readFile('./sample_score.mid', 'base64', function (err,data) {
   // Parse the obtainer base64 string ...
-  var midiArray = midiParser.parse(data);
+  const midiArray = midiParser.parse(data);
   // done!
-  console.log(midiArray);
-  console.log(midiArray.track[0].event);
-  console.log('88 is metric');
-  console.log('81 is tempo : 60000000 / value');
+  const rawTempoList = midiArray.track[0].event
+  const timeDiv = midiArray.timeDivision;
+  let currentMetric = {upper: 4, lower: 4};
+  let currentMeasure = 1;
+  let dataset = [];
+
+  rawTempoList.forEach(line => {
+    // console.log(line);
+    currentMeasure = computeMetricTime(currentMetric.upper, currentMetric.lower, line.deltaTime/timeDiv) + currentMeasure;
+
+
+    switch (line.metaType) {
+      case 6:
+        const thisMarker = {name: line.data};
+        console.log(`Marker. ${thisMarker.name} @measure ${currentMeasure}`);
+        break;
+      case 81:
+        const thisTempo = {tempo: 60000000 / line.data, time: line.deltaTime/960};
+        dataset
+        console.log(`Tempo. ${thisTempo.tempo} @measure ${currentMeasure}`);
+        break;
+      case 88:
+        const thisMetric = {upper: line.data[0], lower: Math.pow(2,line.data[1]), time: line.deltaTime/960};
+        console.log(`Metric. ${thisMetric.upper} ${thisMetric.lower} @measure ${currentMeasure}`);
+        currentMetric = thisMetric;
+        break;
+      default:
+    }
+
+  });
 
 });
