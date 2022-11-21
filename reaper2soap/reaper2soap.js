c84e25964d222975cb74fd3a5918506707156c16
Etienne <etioud@gmail.com>
2022-11-21 18:48:44 +0100

    working on reaper2soap

--- reaper2soap/reaper2soap.js
+++ reaper2soap/reaper2soap.js
@@ -7,20 +7,41 @@ let output = ``;
 function computeMetricTime(prevUpper, prevLower, triggerTime) {
   const beatTime = 4 / prevLower;
   const measureTime = beatTime * prevUpper;
-  const triggerMeasure = (triggerTime / measureTime);
-  return triggerMeasure;
+  const myBar = (triggerTime / measureTime);
+  // const myBeat =
+  return myBar;
 }
 
-function createOutputFile(dataset) {
+function computeBarBeat(upper, lower, floatMeasure) {
+  const myBar = Math.floor(floatMeasure);
+  let myBeat = 1 + ( (floatMeasure - Math.floor(myBar)) * lower );
+  myBeat = Math.round(myBeat*100)/100;
+  return {bar:myBar, beat:myBeat};
+}
+
+function createOutputFile(events) {
   output = ``;
-  Object.keys(dataset).forEach(bar => {
-    if (!dataset[bar].marker) {
-      output += `BAR ${bar} [${dataset[bar].metric.upper}/${dataset[bar].metric.lower}]\n`
+  events.forEach((e) => {
+    output += `BAR ${e.bar}`
+    if (e.signature) {
+      output +=  ` [${e.signature.upper}/${e.signature.lower}]`
+    }
+    //cassé
+    if (e.label) {
+      output += ` "${e.label}"\n`
     }
     else {
-      output += `BAR ${bar} [${dataset[bar].metric.upper}/${dataset[bar].metric.lower}] "${dataset[bar].marker}"\n`
+      output += `\n`
     }
-    output += `| TEMPO ${dataset[bar].tempo} 1/4\n`
+    //cassé
+    if (e.bpm) {
+      if (e.beat !== 1) {
+        output += `| TEMPO ${e.bpm} [1/4]\n`
+      }
+      else {
+        output += `|${e.beat} TEMPO ${e.bpm} [1/4]\n`
+      }
+    }
   });
   return output;
 }
@@ -37,27 +58,39 @@ fs.readFile('./sample_score.mid', 'base64', function (err,data) {
   let currentMeasure = 1;
   let outputFileName = "";
   let thisMetric = {};
-  let dataset = {};
+  let events = [];
+  let index = 0;
 
-  rawTempoList.forEach(line => {
+  rawTempoList.forEach((line) => {
     currentMeasure = computeMetricTime(currentMetric.upper, currentMetric.lower, line.deltaTime/timeDiv) + currentMeasure;
-    if (!dataset[currentMeasure]) {
-      dataset[currentMeasure] = {};
+    currentBarBeat = computeBarBeat(currentMetric.upper, currentMetric.lower, currentMeasure);
+
+    const elemBar = events.find(ev => ev.bar === currentBarBeat.bar);
+    const indexBar = events.findIndex(ev => ev.bar === currentBarBeat.bar);
+
+    if (elemBar === undefined) {
+      index = events.push({bar:currentBarBeat.bar,beat:currentBarBeat.beat});
+      index -= 1;
     }
+    else if (events[indexBar].beat !== currentBarBeat.beat) {
+      index = events.push({bar:currentBarBeat.bar, beat:currentBarBeat.beat});
+      index -=1;
+    }
+
     switch (line.metaType) {
       case 3:
         outputFileName = line.data;
         break;
       case 6:
-        dataset[currentMeasure].marker = line.data;
+        events[index].label = line.data;
         break;
       case 81:
-        const thisTempo = Math.round( (60000000 / line.data) * 100 ) / 100 ;
-        dataset[currentMeasure].tempo = thisTempo;
+        const thisTempo = Math.round( (60000000 / line.data) * 1000 ) / 1000;
+        events[index].bpm = thisTempo;
         break;
       case 88:
         thisMetric = {upper: line.data[0], lower: Math.pow(2,line.data[1])};
-        dataset[currentMeasure].metric = thisMetric;
+        events[index].signature = thisMetric;
         currentMetric = thisMetric;
         break;
       default:
@@ -65,7 +98,7 @@ fs.readFile('./sample_score.mid', 'base64', function (err,data) {
     }
 
   });
-  output = createOutputFile(dataset);
+  output = createOutputFile(events);
   fs.writeFileSync(`${outputFileName}.soap`, output);
   console.log(output);
 });
