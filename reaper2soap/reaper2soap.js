a1f25ff19678a568510227f5c44e6b09e55e928c
Etienne <etioud@gmail.com>
2022-11-22 11:31:47 +0100

    working version with old data structure

--- reaper2soap/reaper2soap.js
+++ reaper2soap/reaper2soap.js
@@ -14,34 +14,40 @@ function computeMetricTime(prevUpper, prevLower, triggerTime) {
 
 function computeBarBeat(upper, lower, floatMeasure) {
   const myBar = Math.floor(floatMeasure);
-  let myBeat = 1 + ( (floatMeasure - Math.floor(myBar)) * lower );
+  let myBeat = 1 + ( (floatMeasure - Math.floor(myBar)) * upper );
   myBeat = Math.round(myBeat*100)/100;
   return {bar:myBar, beat:myBeat};
 }
 
 function createOutputFile(events) {
   output = ``;
+  prevBar = 0;
   events.forEach((e) => {
-    output += `BAR ${e.bar}`
+    if (prevBar !== e.bar) {
+      output += `BAR ${e.bar} `
+    }
     if (e.signature) {
-      output +=  ` [${e.signature.upper}/${e.signature.lower}]`
+      output +=  `[${e.signature.upper}/${e.signature.lower}] `
     }
-    //cassé
-    if (e.label) {
-      output += ` "${e.label}"\n`
+    if (e.label && e.beat === 1) {
+      output += `"${e.label}"`
     }
-    else {
+    if (prevBar !== e.bar) {
       output += `\n`
     }
-    //cassé
-    if (e.bpm) {
-      if (e.beat !== 1) {
-        output += `| TEMPO ${e.bpm} [1/4]\n`
-      }
-      else {
-        output += `|${e.beat} TEMPO ${e.bpm} [1/4]\n`
-      }
+    if (e.beat === 1) {
+      output += `| `
+    } else {
+      output += `|${e.beat} `
     }
+    if (e.bpm) {
+      output += `TEMPO ${e.bpm} [1/4] `
+    }
+    if (e.label && e.beat !== 1) {
+      output += ` "${e.label}"`
+    }
+    output += `\n`
+    prevBar = e.bar;
   });
   return output;
 }
@@ -55,15 +61,15 @@ fs.readFile('./sample_score.mid', 'base64', function (err,data) {
   const rawTempoList = midiArray.track[0].event
   const timeDiv = midiArray.timeDivision;
   let currentMetric = {upper: 4, lower: 4};
-  let currentMeasure = 1;
+  let currentFloatMeasure = 1;
   let outputFileName = "";
   let thisMetric = {};
   let events = [];
   let index = 0;
 
   rawTempoList.forEach((line) => {
-    currentMeasure = computeMetricTime(currentMetric.upper, currentMetric.lower, line.deltaTime/timeDiv) + currentMeasure;
-    currentBarBeat = computeBarBeat(currentMetric.upper, currentMetric.lower, currentMeasure);
+    currentFloatMeasure = computeMetricTime(currentMetric.upper, currentMetric.lower, line.deltaTime/timeDiv) + currentFloatMeasure;
+    currentBarBeat = computeBarBeat(currentMetric.upper, currentMetric.lower, Math.round(currentFloatMeasure*1000)/1000);
 
     const elemBar = events.find(ev => ev.bar === currentBarBeat.bar);
     const indexBar = events.findIndex(ev => ev.bar === currentBarBeat.bar);
@@ -101,6 +107,7 @@ fs.readFile('./sample_score.mid', 'base64', function (err,data) {
   output = createOutputFile(events);
   fs.writeFileSync(`${outputFileName}.soap`, output);
   console.log(output);
+  console.log(events);
 });
 
 
