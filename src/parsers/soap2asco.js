c5046f3e3939baf788fb7f9e545ff2cbc7cc689c
Etienne <etioud@gmail.com>
2023-05-05 15:34:23 +0200

    working on asco

--- src/parsers/soap2asco.js
+++ src/parsers/soap2asco.js
@@ -17,7 +17,7 @@ const soap2asco = {
     fs.writeFileSync(output, soapScore);
   },
 
-  parse: function(score) {
+  parse: function(score, comment = false) {
     const interpreter = new SoapScoreInterpreter(score);
 
     let output = ``;
@@ -50,23 +50,47 @@ const soap2asco = {
       }
 
       const { bar, beat, event, position, duration, dt, unit } = infos;
-      console.log(bar, beat, unit, duration, dt);
+      // console.log(bar, beat, unit, duration, dt);
 
       // if (event.label === 'end-of-score') {
       //   return output;
       // }
+      if (event.duration) {
+        // absolute measure
+        event.tempo = {
+          bpm:60,
+          basis:{
+            upper:1,
+            lower:4,
+          },
+        };
+        // output += `BPM 60\n`
+      }
 
       // check if tempo has changed
-      if (event !== currentEvent && event.tempo.bpm !== currentEvent.tempo.bpm) {
-        const normalizedBPM = event.tempo.bpm * (event.tempo.basis.upper / event.tempo.basis.lower) / (unit.upper / unit.lower);
-        output += `BPM ${normalizedBPM}\n`;
+      if (event !== currentEvent && event.tempo && event.tempo.bpm !== currentEvent.tempo.bpm) {
+        // bpm in antescofo -> related to quarter note
+        const bpm = event.tempo.bpm * (event.tempo.basis.upper / event.tempo.basis.lower) * 4
+        output += `BPM ${bpm}\n`;
       };
 
-      if (bar !== currentBar) {
-        // output += `; ----------- measure ${bar} --- time signature ${event.signature.name}\n`
-        output += `NOTE ${beat+59} ${dt} MEASURE_${bar}`;
+      let dtInBeat;
+      if (event.duration === null) {
+        const { bpm, upper, lower } = unit;
+        const beatDuration = 60 / bpm;
+        dtInBeat = (unit.upper / unit.lower) * 4;
+        dtInBeat = dtInBeat * (dt / beatDuration);
       } else {
-        output += `NOTE ${beat+59} ${dt}`;
+        dtInBeat = event.duration;
+      }
+
+      if (bar !== currentBar) {
+        if (comment === true) {
+          output += `; ----------- measure ${bar} --- time signature ${event.signature.name}\n`
+        }
+        output += `NOTE ${beat+59} ${dtInBeat} MEASURE_${bar}`;
+      } else {
+        output += `NOTE ${beat+59} ${dtInBeat}`;
       }
 
       // check for a label
--- tests/writer-asco.spec.js
+++ tests/writer-asco.spec.js
@@ -12,11 +12,10 @@ describe('soap.writeScore', () => {
     describe('# Basics', () => {
       it(`## Example 1`, () => {
         const data = `\
-BAR 1 [4/4] TEMPO [1/4]=60
-BAR 2 "end-of-score" \
+BAR 1 [4/4] TEMPO [1/4]=90 END \
 `;
         const expected = `\
-BPM 60
+BPM 90
 NOTE 60 1 MEASURE_1
 NOTE 61 1
 NOTE 62 1
@@ -25,31 +24,29 @@ NOTE 63 1\n`;
         const ascoScore = soap2asco.parse(data);
         // console.log(JSON.stringify(ascoScore));
         // console.log(JSON.stringify(expected));
-        console.log(ascoScore);
+        //console.log(ascoScore);
         assert.deepEqual(expected, ascoScore);
       });
       it(`## Example 2`, () => {
         const data = `\
-BAR 1 [6/8] TEMPO [3/8]=60
-BAR 2 "end-of-score" \
+BAR 1 [6/8] TEMPO [3/8]=60 END \
 `;
         const expected = `\
-BPM 60
-NOTE 60 1 MEASURE_1
-NOTE 61 1\n`;
+BPM 90
+NOTE 60 1.5 MEASURE_1
+NOTE 61 1.5\n`;
 
         const ascoScore = soap2asco.parse(data);
-        console.log(ascoScore);
+        //console.log(ascoScore);
         assert.deepEqual(expected, ascoScore);
 
       });
       it(`## Example 2bis`, () => {
         const data = `\
-BAR 1 [6/8] TEMPO [1/8]=120
-BAR 2 "end-of-score" \
+BAR 1 [6/8] TEMPO [1/8]=120 END \
 `;
         const expected = `\
-BPM 40
+BPM 60
 NOTE 60 1.5 MEASURE_1
 NOTE 61 1.5\n`;
 
@@ -60,11 +57,10 @@ NOTE 61 1.5\n`;
       });
       it(`## Example 2ter`, () => {
         const data = `\
-BAR 1 [6/8] TEMPO [1/4]=60
-BAR 2 "end-of-score" \
+BAR 1 [6/8] TEMPO [1/4]=60 END \
 `;
         const expected = `\
-BPM 40
+BPM 60
 NOTE 60 1.5 MEASURE_1
 NOTE 61 1.5\n`;
 
@@ -73,10 +69,9 @@ NOTE 61 1.5\n`;
         assert.deepEqual(expected, ascoScore);
 
       });
-      it.only(`## Example 2quater`, () => {
+      it(`## Example 2quater`, () => {
         const data = `\
-BAR 1 [5/8] TEMPO [1/4]=60
-BAR 2 "end-of-score" \
+BAR 1 [5/8] TEMPO [1/4]=60 END \
 `;
         const expected = `\
 BPM 60
@@ -84,14 +79,13 @@ NOTE 60 1.5 MEASURE_1
 NOTE 61 1\n`;
 
         const ascoScore = soap2asco.parse(data);
-        console.log(ascoScore);
+        //console.log(ascoScore);
         assert.deepEqual(expected, ascoScore);
 
       });
       it('## Example 2quinter', () => {
         const data = `\
-BAR 1 [3+3+2+2/8] TEMPO [1/4]=60
-BAR 2 "end-of-score" \
+BAR 1 [3+3+2+2/8] TEMPO [1/4]=60 END \
 `;
         const expected = `\
 BPM 60
@@ -107,8 +101,7 @@ NOTE 63 1\n`;
       it(`## Example 3`, () => {
         const data = `BAR 1 [4/4] TEMPO [1/4]=120
 BAR 3 [6/4]
-BAR 4 [4/4]
-BAR 5 "end-of-score" \
+BAR 4 [4/4] END \
 `;
         const expected = `\
 BPM 120
@@ -139,8 +132,7 @@ NOTE 63 1\n`;
       it(`## Example 4`, () => {
         const data =`\
 BAR 1 [4/4] TEMPO [1/4]=120
-BAR 3 TEMPO [1/4]=50
-BAR 4 "end-of-score" \
+BAR 3 TEMPO [1/4]=50 END \
 `;
         const expected = `\
 BPM 120
@@ -167,8 +159,7 @@ NOTE 63 1\n`;
       it('# Example 1', () => {
         const data =`\
 BAR 1 10s
-BAR 2 7.5s
-BAR 3 "end-of-score" \
+BAR 2 7.5s END \
 `;
         const expected = `\
 BPM 60
@@ -176,15 +167,14 @@ NOTE 60 10 MEASURE_1
 NOTE 60 7.5 MEASURE_2\n`;
 
         const ascoScore = soap2asco.parse(data);
-        //console.log(ascoScore);
+        // console.log(ascoScore);
         assert.deepEqual(expected, ascoScore);
       });
       it('# Example 2', () => {
         const data =`\
 BAR 1 [4/4] TEMPO [1/4]=80
 BAR 2 10s
-BAR 3 [3/4]
-BAR 4 "end-of-score" \
+BAR 3 [3/4] END \
 `;
 
         const expected = `\
@@ -201,7 +191,7 @@ NOTE 61 1
 NOTE 62 1\n`;
 
         const ascoScore = soap2asco.parse(data);
-        //console.log(ascoScore);
+        // console.log(ascoScore);
         assert.deepEqual(expected, ascoScore);
       })
     });
@@ -209,8 +199,7 @@ NOTE 62 1\n`;
       it(`## Example 1`, () => {
         const data =`\
 BAR 1 [4/4] TEMPO [1/4]=120 "section A"
-BAR 2 "section B"
-BAR 3 "end-of-score" \
+BAR 2 "section B" END \
 `;
         const expected = `\
 BPM 120
@@ -230,8 +219,7 @@ NOTE 63 1\n`;
       it(`## Example 1bis`, () => {
         const data =`\
 BAR 1 [4/4] TEMPO [1/4]=120
-BAR 3 TEMPO [1/4]=50
-BAR 4 "end-of-score" \
+BAR 3 TEMPO [1/4]=50 END \
 `;
         const expected = `\
 BPM 120
@@ -257,8 +245,7 @@ NOTE 63 1\n`;
         const data =`\
 BAR 1 [4/4] TEMPO [1/4]=120 "section A"
 BAR 2 [3/4]
-BAR 3 "section B"
-BAR 4 "end-of-score" \
+BAR 3 "section B" END \
 `;
         const expected = `\
 BPM 120
@@ -278,12 +265,12 @@ NOTE 62 1\n`;
         assert.deepEqual(expected, ascoScore);
       });
     });
-    describe('# Bar Subdivisions', () => {
+    describe.only('# Bar Subdivisions', () => {
       it(`## Example 1`, () => {
         const data =`\
 BAR 1 [4/4] TEMPO [1/4]=120 "To Flute"
 |3 "To Piccolo"
-BAR 2 "end-of-score" \
+|4 END \
 `;
         const expected = `\
 BPM 120
@@ -293,14 +280,13 @@ NOTE 62 1 "To Piccolo"
 NOTE 63 1\n`;
 
         const ascoScore = soap2asco.parse(data);
-        //console.log(ascoScore);
+        // console.log(ascoScore);
         assert.deepEqual(expected, ascoScore);
       });
       it(`## Example 2`, () => {
         const data =`\
 BAR 1 [4/4] TEMPO [1/4]=120
-|4.5 "accent"
-BAR 2 "end-of-score" \
+|4.5 "accent" END \
 `;
         const expected = `\
 BPM 120
@@ -311,7 +297,7 @@ NOTE 63 0.5
 NOTE 0 0.5 "accent"\n`;
 
         const ascoScore = soap2asco.parse(data);
-        //console.log(ascoScore);
+        console.log(ascoScore);
         assert.deepEqual(expected, ascoScore);
       });
     });
@@ -321,8 +307,7 @@ NOTE 0 0.5 "accent"\n`;
 BAR 1 [4/4] TEMPO [1/4]=120
 BAR 3
 |3 FERMATA [1/2]=10s
-BAR 4
-BAR 5 "end-of-score" \
+BAR 4 END \
 `;
         const expected = `\
 BPM 120
@@ -350,7 +335,8 @@ NOTE 63 1\n`;
         const data =`\
 BAR 1 [4/4]
 |1 TEMPO [1/4]=120
-|2 FERMATA [3/8]=? \
+|2 FERMATA [3/8]=?
+|3 END \
 `;
         const expected = `\
 BPM 120
@@ -368,7 +354,7 @@ NOTE 63 1 \
 BAR 1 [4/4]
 |1 TEMPO [1/4]=120
 |2 FERMATA [3/8]=2* \
-`;
+|3 END`;
         const expected = `\
 BPM 120
 NOTE 60 1 MEASURE_1
@@ -385,8 +371,7 @@ NOTE 63 1 \
         const data =`\
 BAR 1 [3+2+2/8] TEMPO [3/8]=60
 BAR 2 [2+3+2/8]
-BAR 3 [2+2+3/8]
-BAR 4 "end-of-score"\
+BAR 3 [2+2+3/8] END\
 `;
         const expected = `\
 BPM 90
@@ -410,8 +395,7 @@ NOTE 62 1.5\n`;
         const data =`\
 BAR 1 [4/4] TEMPO [1/4]=80
 BAR 2 TEMPO [1/4]=80 curve 1
-BAR 3 TEMPO [1/4]=120
-BAR 4 "end-of-score" \
+BAR 3 TEMPO [1/4]=120 END \
 `;
         const expected = `\
 BPM 80
