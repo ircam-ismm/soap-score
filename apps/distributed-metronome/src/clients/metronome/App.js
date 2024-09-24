0133356bdf2d22a524a65c71abd66102d02600e8
b-ma <benjamin.matuszewski@gmail.com>
2024-09-24 16:49:32 +0200

    fix: working distributed version

--- apps/distributed-metronome/src/clients/metronome/App.js
+++ apps/distributed-metronome/src/clients/metronome/App.js
@@ -1,5 +1,5 @@
 import { Scheduler, Transport } from '@ircam/sc-scheduling';
-import { html, render } from 'lit';
+import { html, render, nothing } from 'lit';
 
 import SoapScoreInterpreter from '../../../../../src/SoapScoreInterpreter.js';
 
@@ -27,6 +27,7 @@ class App {
     this.audioContext = audioContext;
     this.buffers = buffers;
     this.defaultScore = defaultScore;
+    this.renderAdvancedOptions = false;
 
     this.interpreter = null;
     this.score = null;
@@ -64,66 +65,105 @@ class App {
     this.render();
   }
 
-  // @todo - provide several layouts
   render() {
     render(html`
-      <div>
-        <sc-clock
-          .getTimeFunction=${() => this.transport.currentPosition}
-        ></sc-clock>
-        <br />
-        <soap-transport-control
-          .global=${this.global}
-          .transport=${this.transport}
-          .interpreter=${this.interpreter}
-          .audioContext=${this.audioContext}
-          score=${this.score}
-        ></soap-transport-control>
+      <header>
+        <div>
+          <img src="./assets/logo-200x200.png" />
+          <h1>SO(a)P Metronome</h1>
+           <a
+            href="https://github.com/ircam-ismm/soap-score/blob/main/docs/SYNTAX.md"
+            target="_blank"
+          >Syntax documentation</a>
+        </div>
+        <div style="font-size: 0;">
+          <sc-clock format="hh:mm:ss"></sc-clock>
+          <sc-icon type="github" href="https://github.com/ircam-ismm/soap-score"></sc-icon>
+          <sc-icon
+            type="burger"
+            @input=${e => {
+              this.renderAdvancedOptions = !this.renderAdvancedOptions;
+              this.render();
+            }}
+          ></sc-icon>
+        </div>
+      </header>
+      <div id="full">
+        <div class="row-1">
+          <div class="col-1">
+            <div>
+              <soap-stave-renderer
+                .transport=${this.transport}
+                .interpreter=${this.interpreter}
+              ></soap-stave-renderer>
+              <soap-flash-beat-renderer
+                .transport=${this.transport}
+                .interpreter=${this.interpreter}
+              ></soap-flash-beat-renderer>
+            </div>
+            <soap-progress-beat-renderer
+              .transport=${this.transport}
+              .interpreter=${this.interpreter}
+            ></soap-progress-beat-renderer>
+          </div>
+          <div class="col-2">
+            <sc-clock
+              .getTimeFunction=${() => this.transport.currentPosition}
+            ></sc-clock>
+            <soap-score-location-renderer
+              .transport=${this.transport}
+              .interpreter=${this.interpreter}
+            ></soap-score-location-renderer>
+          </div>
+        </div>
+        <div class ="row-2">
+          <div class="col-1">
+            <soap-score-generator
+              @change=${e => this.setScore(e.detail.value)}
+            ></soap-score-generator>
+            <soap-transport-control
+              .global=${this.global}
+              .transport=${this.transport}
+              .interpreter=${this.interpreter}
+              .audioContext=${this.audioContext}
+              score=${this.score}
+            ></soap-transport-control>
+            <soap-metronome-renderer
+              .transport=${this.transport}
+              .interpreter=${this.interpreter}
+              .audioContext=${this.audioContext}
+              .buffers=${this.buffers}
+              .audioOutput=${this.audioContext.destination}
+              sonification=${this.sonification}
+              sonificationMode=${this.sonificationMode}
+              @change=${e => this.updateSonification(e.detail.key, e.detail.value)}
+            ></soap-metronome-renderer>
+            <soap-score-export
+              .score=${this.score}
+              .interpreter=${this.interpreter}
+              .buffers=${this.buffers}
+              sonification=${this.sonification}
+              sonificationMode=${this.sonificationMode}
+            ></soap-score-export>
+          </div>
+          <soap-score-editor class="col-2"
+            score=${this.score}
+            @change=${e => this.setScore(e.detail.value)}
+          ></soap-score-editor>
+        </div>
 
-        <br />
-        <soap-flash-beat-renderer
-          .transport=${this.transport}
-          .interpreter=${this.interpreter}
-        ></soap-flash-beat-renderer>
-        <br />
-        <soap-score-location-renderer
-          .transport=${this.transport}
-          .interpreter=${this.interpreter}
-        ></soap-score-location-renderer>
-        <br />
-        <soap-stave-renderer
-          .transport=${this.transport}
-          .interpreter=${this.interpreter}
-        ></soap-stave-renderer>
-        <br >
-        <soap-score-editor
-          score=${this.score}
-          @change=${e => this.setScore(e.detail.value)}
-        ></soap-score-editor>
-        <br >
-        <soap-metronome-renderer
-          .transport=${this.transport}
-          .interpreter=${this.interpreter}
-          .audioContext=${this.audioContext}
-          .buffers=${this.buffers}
-          .audioOutput=${this.audioContext.destination}
-        ></soap-metronome-renderer>
-        <br />
-        <soap-score-examples
-          @change=${e => this.setScore(e.detail.value)}
-        ></soap-score-examples>
-        <br />
-        <soap-score-generator
-          @change=${e => this.setScore(e.detail.value)}
-        ></soap-score-generator>
-        <br />
-        <soap-score-import
-          @change=${e => this.setScore(e.detail.value)}
-        ></soap-score-import>
-        <br />
-        <soap-score-export
-          .score=${this.score}
-        ></soap-score-export>
+        ${this.renderAdvancedOptions
+          ? html`
+            <div class="advanced-options">
+              <soap-score-examples
+                @change=${e => this.setScore(e.detail.value)}
+              ></soap-score-examples>
+              <soap-score-import
+                @change=${e => this.setScore(e.detail.value)}
+              ></soap-score-import>
+            </div>`
+          : nothing
+        }
       </div>
     `, this.$container);
   }
